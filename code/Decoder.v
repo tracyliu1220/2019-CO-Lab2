8f5ee04fa6f6f1384bbacd642e164d43b7453a8d
Chieh Nien <Jeffnien123321@gmail.com>
2019-05-01 14:10:53 +0800

    ori modified

--- code/Decoder.v
+++ code/Decoder.v
@@ -82,7 +82,7 @@ always@(*) begin
     6'b001011: SE_o <= 0;
     6'b000100: SE_o <= 1;
     6'b001111: SE_o <= 1;
-    6'b001101: SE_o <= 1;
+    6'b001101: SE_o <= 0;
     6'b000101: SE_o <= 1;
   endcase
   case (funct)
